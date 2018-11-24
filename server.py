fdd1e5e8ec842186280c41288e216022abc3edce
kirkbyo <ozzie.kirkby@gmail.com>
2018-11-24 14:02:14 -0500

    tmp

--- server.py
+++ server.py
@@ -2,10 +2,16 @@ from flask import Flask
 import os
 app = Flask(__name__, static_url_path='')
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def root():
-    return app.send_static_file('./webpage.html')
+    if request.method == 'POST':
+        return
+    else:
+        return app.send_static_file('./webpage.html')
 
+@app.route("result")
+def root():
+    return app.send_static_file('./results.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
