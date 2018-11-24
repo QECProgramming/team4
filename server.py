09d50969e5a1d91daa476ce496c7552fc356ae20
kirkbyo <ozzie.kirkby@gmail.com>
2018-11-24 13:59:13 -0500

    Add Statics folder

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
+import os
+app = Flask(__name__, static_url_path='')
 
-@app.route("/search")
-def search():
-    return "Hello World!"
+@app.route("/")
+def root():
+    return app.send_static_file('./webpage.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
